04fd116cca4578850dcb17f0c86d86d82353fd2e
MacBobby Chibuzor <theghostmac@gmail.com>
2022-06-17 22:51:05 +0100

    random dinner selection

--- GolangReview/prodGopher.go
+++ GolangReview/prodGopher.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	dinners := []string{
+		"chicken and chips",
+		"fufu and egusi",
+		"yam and egg sauce",
+		"noodles and turkey",
+	}
+
+	rand.Seed(time.Now().Unix())
+
+	fmt.Printf("I had %s for dinner tonight!", dinners[rand.Intn(len(dinners))])
+}
