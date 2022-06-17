ac9560c7194a50caa5551dcd37fdf9ec088c175a
MacBobby Chibuzor <theghostmac@gmail.com>
2022-06-17 22:54:48 +0100

    mastered the random string selector code

--- GolangReview/prodGopher.go
+++ GolangReview/prodGopher.go
@@ -7,14 +7,13 @@ import (
 )
 
 func main() {
-	dinners := []string{
-		"chicken and chips",
+	myDinner := []string{
 		"fufu and egusi",
+		"chicken and chips",
 		"yam and egg sauce",
 		"noodles and turkey",
 	}
 
 	rand.Seed(time.Now().Unix())
-
-	fmt.Printf("I had %s for dinner tonight!", dinners[rand.Intn(len(dinners))])
+	fmt.Printf("I had %s for dinner tonight!", myDinner[rand.Intn(len(myDinner))])
 }
