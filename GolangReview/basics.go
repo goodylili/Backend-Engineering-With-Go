7a005e157d149a8590cf235fd14d427d1a5a63d4
MacBobby Chibuzor <theghostmac@gmail.com>
2022-06-19 13:24:39 +0100

    structs

--- GolangReview/basics.go
+++ GolangReview/basics.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+// Naming variables (same as constants)
+//var x int
+//var y, z int
+//var (
+//	a int
+//	b string
+//	c float64
+//)
+
+type PersonalID struct {
+	name   string
+	number int
+	age    int
+	isMale bool
+}
+
+func main() {
+	person1 := PersonalID{
+		"Uzor",
+		0001,
+		15,
+		true,
+	}
+
+	fmt.Println("First candidate: ", person1)
 }
