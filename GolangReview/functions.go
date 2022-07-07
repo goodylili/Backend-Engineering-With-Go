4b22b5b9a6672a104cf4eec9c0a83506e557fe37
MacBobby Chibuzor <theghostmac@gmail.com>
2022-07-07 11:36:13 +0100

    multiple return values

--- GolangReview/functions.go
+++ GolangReview/functions.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func div(numerator, denominator int) int {
 	if denominator == 0 {
@@ -9,7 +13,21 @@ func div(numerator, denominator int) int {
 	return numerator / denominator
 }
 
+func divAndRem(numerator, denominator int) (int, int, error) {
+	if denominator == 0 {
+		return 0, 0, errors.New("cannot be divided by zero")
+	}
+	return numerator / denominator, numerator % denominator, nil
+}
+
 func main() {
 	result := div(20, 10)
 	fmt.Println(result)
+
+	res, remainder, err := divAndRem(50, 5)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(res, remainder)
 }
