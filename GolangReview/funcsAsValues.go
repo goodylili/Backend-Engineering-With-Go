987818e261b15c70c61d9121beb60bff7accdaa3
MacBobby Chibuzor <theghostmac@gmail.com>
2022-07-07 11:54:48 +0100

    calculator program

---
+++ GolangReview/funcsAsValues.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	if x > y {
+		return x - y
+	} else {
+		return y - x
+	}
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	if x == 0 && y == 0 {
+		return 0
+	} else {
+		return x / y
+	}
+}
+
+var operatorMap = map[string]func(int, int) int{
+	"+": add,
+	"-": subtract,
+	"*": multiply,
+	"/": divide,
+}
+
+func main() {
+	mathExpressions := [][]string{
+		[]string{"2", "+", "3"},
+		[]string{"2", "-", "3"},
+		[]string{"2", "*", "3"},
+		[]string{"6", "/", "3"},
+		[]string{"5"},
+		[]string{"two", "+", "three"},
+	}
+
+	for _, expressions := range mathExpressions {
+		if len(expressions) != 3 {
+			fmt.Println("invalid expression: ", expressions)
+			continue
+		}
+		p1, err := strconv.Atoi(expressions[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		op := expressions[1]
+		operatorFunc, ok := operatorMap[op]
+		if !ok {
+			fmt.Println("unsupported operator: ", op)
+			continue
+		}
+		p2, err := strconv.Atoi(expressions[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		result := operatorFunc(p1, p2)
+		fmt.Println(result)
+	}
+}
