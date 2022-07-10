900fc049bf2bfaa0214cccd591201380c8d68027
MacBobby Chibuzor <theghostmac@gmail.com>
2022-07-10 15:25:58 +0100

    counter.go | pass by value & reference

--- GolangReview/counter.go
+++ GolangReview/counter.go
@@ -10,7 +10,7 @@ type Counter struct {
 	lastUpdated time.Time
 }
 
-// pointer receiver method
+// pointer receiver method used because I am modifying (also necessary when handling nil instances)
 
 func (c *Counter) Increment() {
 	c.total++
@@ -23,9 +23,24 @@ func (c Counter) MakeString() string {
 	return fmt.Sprintf("total: %d, last updated: %v", c.total, c.lastUpdated)
 }
 
+func doUpdateWrong(c Counter) {
+	c.Increment()
+	fmt.Println("in doingUpdateWrong: ", c.MakeString())
+}
+
+func doUpdateRight(c *Counter) {
+	c.Increment()
+	fmt.Println("in doingUpdateRight:", c.MakeString())
+}
+
 func main() {
 	var c Counter
 	fmt.Println(c.MakeString())
 	c.Increment()
 	fmt.Println(c.MakeString())
+
+	doUpdateWrong(c)
+	fmt.Println("in main of doUpdateWrong: ", c.MakeString())
+	doUpdateRight(&c)
+	fmt.Println("in main of doUpdateRight: ", c.MakeString())
 }
