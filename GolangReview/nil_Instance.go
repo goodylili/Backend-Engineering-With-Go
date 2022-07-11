cf26806cff3ee6280a1c1c9773f70308665e3cde
MacBobby Chibuzor <theghostmac@gmail.com>
2022-07-11 23:30:30 +0100

    nil interfaces | nil_Instance.go

--- GolangReview/nil_Instance.go
+++ GolangReview/nil_Instance.go
@@ -10,4 +10,10 @@ func (intTree *IntTree) Insert(value int) *IntTree {
 	if intTree == nil {
 		return &IntTree{value: value}
 	}
+	if value < intTree.value {
+		intTree.left = intTree.left.Insert(value)
+	} else if value > intTree.value {
+		intTree.right = intTree.right.Insert(value)
+	}
+	return intTree
 }
